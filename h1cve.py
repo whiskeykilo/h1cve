c7e9c84150907917f37b658827ce93aa92b6a334
Will Kapcio <18176030+whiskeykilo@users.noreply.github.com>
2021-02-23 02:52:49 -0500

    Version 1, ready to test

--- h1cve.py
+++ h1cve.py
@@ -1,53 +1,69 @@
-import requests  # Requests is a http library written for humans
-import json  # JSON is a Python inbuilt library that makes it easy to work with json.
-import tweepy  # Tweepy is a Twitter API wrapper
-import time
+import tweepy
+import requests
+import json
+import os
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from extract import json_extract
+from time import sleep
 
+# get environment variables from .env
+load_dotenv()
 
-# Visit apps.twitter.com to get the following variables
-consumer_key = "consumer_key"
-consumer_secret = "consumer_secret"
-access_token = "access_token"
-access_token_secret = "access_token_secret"
+# try the OS variables just in case (for Heroku)
+consumer_key = os.getenv("API_KEY")
+consumer_secret_key = os.getenv("API_SECRET_KEY")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-
-# Twitter requires oAuth2 to access its API
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)  # This creates an object called api
+twitta = tweepy.API(auth)
 
+# get current time and adjust timedelta to script cron period
+adjusted_date_time = datetime.now() - timedelta(hours=1)
+nvd_date_time = adjusted_date_time.strftime(
+    "%Y-%m-%dT%H:%M:%S:000 UTC-05:00"
+)  # NVD API needs: yyyy-MM-dd'T'HH:mm:ss:SSS z
 
-# National Vulnerability Database (NVD) API
+# National Vulnerability Database (NVD) API documented here: https://bit.ly/3bqcxYk
 api_url = "https://services.nvd.nist.gov/rest/json/cves/1.0"
+site_url = "https://nvd.nist.gov/vuln/detail/"
+
+# NVD API search parameters
+params = {
+    "keyword": "hackerone",  # search for keyword "hackerone"
+    "startIndex": 0,  # start at most recent CVEs
+    "resultsPerPage": 50,  # page big enough for all results at once
+    "pubStartDate": nvd_date_time,
+}
 
 
 def get_cves():
-    """This function gets new CVEs from the NVD API
-    :return: It returns a dictionary object
     """
-    headers = {"X-Mashape-Host": api_url}
-    response = requests.get(api_url, headers=headers)
-    json_converted_dictionary = json.loads(response.content.decode())
-    json_converted_dictionary = (
-        json_converted_dictionary["cve"] + " --" + json_converted_dictionary["author"]
-    )
-    return json_converted_dictionary
+    This function pulls CVEs from NVD in the specified time period and order
+    :return: It returns a list
+    """
+    response = requests.get(api_url, params=params)
+    global id_list
+    id_list = json_extract(response.json(), "ID")
 
 
-def tweet_cve():
-    """This function tweets a new CVE.
-    It tweets CVEs from the get_cves() function.
+def tweet_cves():
+    """
+    This function updates the Twitter timeline with discovered CVEs
     :return: It has no return value
     """
-    for i in range(1, 10):
-        cve = get_cves()
-        time.sleep(30)
+    for i in id_list:
+        tweet = i + " reported via @Hacker0x01 has been published: " + site_url + i
         try:
-            api.update_status(cve)  # This method is accessed in tweepy
-            print("Done")
+            twitta.update_status(tweet)
+            sleep(5)
         except tweepy.error.TweepError:
             pass
 
 
 if __name__ == "__main__":
-    tweet_cve()
+    get_cves()
+    tweet_cves()
