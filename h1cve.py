abd68d55539de65353902b2b039a0224158381b8
Will Kapcio <18176030+whisk3ykilo@users.noreply.github.com>
2022-01-06 14:34:46 -0500

    Timeouts error out quicker in case NVD is down

--- h1cve.py
+++ h1cve.py
@@ -13,51 +13,36 @@ load_dotenv()
 
 # try the OS variables (just in case for Heroku)
 nvd_key = os.environ.get("NVD_KEY")
-consumer_key = os.environ.get("API_KEY")
-consumer_secret_key = os.environ.get("API_SECRET_KEY")
-access_token = os.environ.get("ACCESS_TOKEN")
-access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+consumer_key = os.environ.get("TWITTER_API_KEY")
+consumer_secret_key = os.environ.get("TWITTER_API_SECRET_KEY")
+access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
-# Authenticate to Twitter
+# load Twitter variables
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
 auth.set_access_token(access_token, access_token_secret)
-twitta = tweepy.API(auth)
 
-# get current time and adjust timedelta to script cron period
-datetime_now = datetime.now()
-adjusted_date_time = datetime_now - timedelta(hours=1)
-# adjusted_date_time = datetime_now - timedelta(days=7)  # testing
+# Use to adjust the last_modified_date filter (default last 60 seconds)
+minutes = int(os.environ.get("MINUTES"))
 
-nvd_datetime = adjusted_date_time.strftime(
-    "%Y-%m-%dT%H:%M:%S:000 UTC-05:00"
-)  # NVD API needs: yyyy-MM-dd'T'HH:mm:ss:SSS z
-
-nvd_datetime_now = datetime_now.strftime(
-    "%Y-%m-%dT%H:%M:%S:000 UTC-05:00"
-)  # NVD API needs: yyyy-MM-dd'T'HH:mm:ss:SSS z
+# Adjust the following two to equal 600 seconds (min Scheduler job) - SLEEP in seconds
+sleep = int(os.environ.get("SLEEP"))
 
 # National Vulnerability Database (NVD) API documented here: https://bit.ly/3bqcxYk
 API_URL = "https://services.nvd.nist.gov/rest/json/cves/1.0"
 SITE_URL = "https://nvd.nist.gov/vuln/detail/"
 
-# NVD API search parameters
-params = {
-    "keyword": "hackerone",  # search for keyword "hackerone"
-    "startIndex": 0,  # start at most recent CVEs
-    "resultsPerPage": 100,  # page big enough for all results at once
-    "pubStartDate": nvd_datetime,
-    "pubEndDate": nvd_datetime_now,
-}
-
 
 def requests_retry_session(
-    retries=10,
-    backoff_factor=0.5,
+    retries=3,
+    backoff_factor=0.3,
     status_forcelist=(500, 502, 504),
     session=None,
 ):
     """
-    Retry request function. NVD API can be rather unresponsive
+    Retry request function. NVD API can be rather unresponsive:
+    https://www.peterbe.com/plog/best-practice-with-retries-with-requests
+    :return: Returns the session details
     """
     session = session or requests.Session()
     retry = Retry(
@@ -73,19 +58,52 @@ def requests_retry_session(
     return session
 
 
-def get_cves():
+def login():
+    """
+    Authenticate to Twitter
+    :return: It has no return value
+    """
+    twitta = tweepy.API(auth)
+
+
+def poll_nvd():
     """
     This function pulls CVEs from NVD in the specified time period and order
     :return: It returns a list
     """
-    print("starting get_CVEs\n")
+    print("polling the NVD\n")
 
+    # get current time
+    datetime_now = datetime.now()
+
+    # adjust timedelta to script cron period
+    # adjusted_date_time = datetime_now - timedelta(hours=1) # prod
+    adjusted_date_time = datetime_now - timedelta(days=10)  # test
+
+    nvd_datetime = adjusted_date_time.strftime(
+        "%Y-%m-%dT%H:%M:%S:000 UTC-05:00"
+    )  # NVD API needs: yyyy-MM-dd'T'HH:mm:ss:SSS z
+
+    nvd_datetime_now = datetime_now.strftime(
+        "%Y-%m-%dT%H:%M:%S:000 UTC-05:00"
+    )  # NVD API needs: yyyy-MM-dd'T'HH:mm:ss:SSS z
+
+    # NVD API search parameters
+    params = {
+        "keyword": "hackerone",  # search for keyword "hackerone"
+        "startIndex": 0,  # start at most recent CVEs
+        "resultsPerPage": 50,  # page big enough for all results at once
+        "pubStartDate": nvd_datetime,
+        "pubEndDate": nvd_datetime_now,
+    }
     try:
-        response = requests_retry_session().get(API_URL, params=params, timeout=10)
+        response = requests_retry_session().get(
+            API_URL, params=params, timeout=(3.05, 10)
+        )
         response.raise_for_status()
         print("NVD API status code: " + str(response.status_code) + "\n")
     except requests.exceptions.HTTPError as errh:
-        print("HTTP Error:", errh)
+        print("HTTP Error " + str(response.status_code) + ":\n", errh)
     except requests.exceptions.ConnectionError as errc:
         print("Error Connecting:", errc)
     except requests.exceptions.Timeout as errt:
@@ -99,15 +117,15 @@ def get_cves():
     url_list = json_extract(response.json(), "url")
     h1_url_list = [i for i in url_list if "hackerone" in i]
     MASTER_DICT = dict(zip(id_list, h1_url_list))
-    print("end get_CVEs\n")
+    print("done polling the NVD\n")
 
 
 def tweet_cves():
     """
-    This function updates the Twitter timeline with discovered CVEs
+    This function updates the bot's Twitter timeline with discovered CVEs
     :return: It has no return value
     """
-    print("printing CVEs found here:\n")
+    print("printing discovered CVEs here:\n")
     for cve, h1_url in MASTER_DICT.items():
         tweet = (
             cve
@@ -118,9 +136,9 @@ def tweet_cves():
             + h1_url
         )
         try:
-            twitta.update_status(tweet)
-            print(tweet)
-            sleep(2)
+            # twitta.update_status(tweet)
+            print(tweet + "\n\n" + "-------------------" + "\n")
+            sleep(1)
         except tweepy.errors.HTTPException as err:
             print("Twitter error: ", err.response.text)
 
@@ -128,7 +146,8 @@ def tweet_cves():
 if __name__ == "__main__":
     while True:
         try:
-            get_cves()
+            login()
+            poll_nvd()
             tweet_cves()
             print("\ndone")
         except Exception as exc:
